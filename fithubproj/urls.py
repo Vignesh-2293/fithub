fdc26b96919d0ae5e31d530f516b3250eebcadf1
Vignesh-2293 <96306761+Vignesh-2293@users.noreply.github.com>
2023-12-30 14:48:12 +0530

    urlupdate

--- fithubproj/urls.py
+++ fithubproj/urls.py
@@ -18,13 +18,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path,include
-from django.views.static import serve
-from django.conf.urls import url
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('ftapp.urls')),
-    url(r'^media/(?P<path>.*)$',serve,{'document_root':settings.MEDIA_ROOT}),
+   
 ]
 
 if settings.DEBUG:
